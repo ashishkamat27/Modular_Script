9e9e61d26dddbb87046172018426e3d0ad5f5133
root <root@rider.cybage.com>
2015-05-25 19:01:03 +0530

    Modified deploymnent script

--- ecommerce-deploy.sh
+++ ecommerce-deploy.sh
@@ -7,7 +7,7 @@ DESTINATION=$3
 echo "###########################################################"
 echo "Taking artifact and extactacting into Web Server Directory"
 echo "###########################################################"
-cp -r ecommerce-cd.zip $3
+cp  ecommerce-cd.zip $3
 echo "Wait!! Let me go to web server directory"
 cd $DESTINATION
 unzip $SOURCE_ZIP  $DESTINATION  #given here static path can be changed later
