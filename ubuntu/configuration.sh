f18245a7645775bd8745736922e5bda8016b704d
ashishkamat <ashishka@cybage.com>
2015-07-09 20:40:49 +0530

    Added Deployment Script and Vhost script for Ubutu

--- ubuntu/configuration.sh
+++ ubuntu/configuration.sh
@@ -11,9 +11,10 @@ if   	! which php > /dev/null; then
    	echo "Installinig Php and php-mysql..."
   	sleep 5
   	apt-get clean all
-  	 apt-get -y install php5-cli php5-common php5-mysql
+  	 apt-get -y install php5-cli php5-common php5-mysql php5-xdebug libapache2-mod-php5
 else 
 	echo "Php is installed."
+	php -v
 	printf "\n\n"
 	sleep 5
 fi
@@ -49,12 +50,14 @@ else
 	echo "MySQL is installed."    
        if [ $# -eq 0 ]; then 
 		echo "Mysql password not supplied using default password"
+		mysql --version
 		/etc/init.d/mysql start
 		#mysql -u root -p$ROOTPW
  		printf "\n\n"
 		#exit
       else 
 	 	echo "Mysql password is supplied using given password"
+		mysql --version
 		 /etc/init.d/mysql start
 		#mysql -u root -p$2
  		printf "\n\n"
@@ -71,6 +74,7 @@ if   	! which apache2 > /dev/null; then
 	/etc/init.d/apache2 start
 else 
 	echo "Apache is installed."
+	apache2 -v
 	/etc/init.d/apache2 start
 	printf "\n\n"
 	sleep 5
---
+++ ubuntu/ecommerce-deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#################################Variable Declaration###############################
+SOURCE_ZIP=$2
+DESTINATION=$3
+####################### Unzip File into specified directory#########################
+echo "###########################################################"
+echo "Taking artifact and extactacting into Web Server Directory"
+echo "###########################################################"
+#cp  ecommerce-cd.zip  /opt/rh/httpd24/root/var/www/html
+echo "Wait!! Let me go to web server directory"
+pwd
+#if 	[ -d /opt/rh/httpd24/root/var/www/html/ecommerce-cd ]; then 
+#	echo "Directory exist no need to unzip";
+#else 
+	rm -rf /var/www/html/ecomm_project
+	unzip -d /var/www/html/  ../ecomm_project.zip   #given here static path can be changed later
+#fi 
+cd /var/www/html/
+sleep 5
+echo "Giving executable permission to artifact"
+chmod -R 777 ecomm_project
+echo "###################################################"
+echo " Importing Database"
+echo "##################################################"
+cd ecomm_project/sql 
+pwd
+mysql -uroot -proot -e "DROP DATABASE IF EXISTS ecomm"
+mysql -uroot -proot <ecomm.sql
+mysql -uroot -proot -e "Show databases"
+echo "###################################################"
+echo " Adding Host Entry in hosts file"
+echo "##################################################"
+echo  "127.0.0.1 ecomm_project">>/etc/hosts
+echo "##################################################"
+echo "Restart Apache"
+echo "######################################################"
+ /etc/init.d/apache2 reload
+rm -rf ecomm_project.zip
+echo "success Check your site by typing ecommerce-cd in browser"
+echo "###############################################"
+
