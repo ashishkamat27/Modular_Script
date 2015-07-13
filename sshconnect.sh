7226d1f523b82d1a83a197f9daa88fe9bda995ab
ashishkamat <ashishka@cybage.com>
2015-07-13 02:34:23 -0700

    Added Logic to identify OS in Master Script v0.1

--- sshconnect.sh
+++ sshconnect.sh
@@ -7,5 +7,14 @@ do
 	printf "********************\n\n";
 	export SSHPASS=$password
 	sshpass -e scp -r Scripts $username@$ip:/home/$username/Desktop; 
+	if (cat /etc/*-release|grep -iq CentOS)
+	then
+		echo "Centos";
+	elif (cat /etc/*-release|grep -iq Ubuntu)
+	then
+		echo "Ubuntu";
+	else 
+		echo "Some other";
+	fi
 	#echo "Hello World from Ashish Kamat">>/home/$username/Desktop/; exit
 done  <$FILENAME
