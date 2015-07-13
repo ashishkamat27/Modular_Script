cc4c55acc4ef55539b6552e034fc0faeafa64fa9
ashishkamat <ashishka@cybage.com>
2015-07-13 01:54:50 -0700

    Added Host file having credentials and Script to read entries and connect to machine remotely

---
+++ sshconnect.sh
@@ -0,0 +1,11 @@
+FILENAME=$1;
+while read -r ip username password
+do
+	printf "Connecting to %s"$ip;
+	printf "With username %s"$username;
+	printf "Having Password %s "$password
+	printf "********************\n\n";
+	export SSHPASS=$password
+	sshpass -e scp -r temp $username@$ip:/home/$username/Desktop; 
+	#echo "Hello World from Ashish Kamat">>/home/$username/Desktop/; exit
+done  <$FILENAME
