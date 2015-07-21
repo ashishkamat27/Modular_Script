ea489be8fb43e840aaeb0f083e52c085fd0bd3e8
ashishkamat <ashishka@cybage.com>
2015-07-21 07:44:50 -0700

    Merging

--- Scripts/ubuntu/ecommerce-deploy.sh
+++ Scripts/ubuntu/ecommerce-deploy.sh
@@ -36,7 +36,27 @@ echo  "127.0.0.1 ecomm_project">>/etc/hosts
 echo "##################################################"
 echo "Virtual Host Configuration"
 echo "###################################################"
-	sh ecommerce-vhost.sh
+if [ -f /etc/apache2/sites-available/ecomm_project.conf ]; then
+echo  "File is already exist"
+else
+echo  '<VirtualHost *:80>
+    		<Directory "/var/www/html/ecomm_project">
+        	Options Indexes FollowSymLinks Includes ExecCGI
+        	Order allow,deny
+        	Allow from all
+    		</Directory>
+    		<IfModule mod_php5.c>
+        		php_admin_flag safe_mode Off
+        		php_admin_value register_globals 0
+        		php_value magic_quotes_gpc 0
+        		php_value magic_quotes_runtime 0
+        		php_value allow_call_time_pass_reference 0
+   		</IfModule>
+    		DirectoryIndex index.php
+    		DocumentRoot "/var/www/html/ecomm_project"
+    		ServerName ecomm_project
+   	</VirtualHost>'>>/etc/apache2/sites-available/ecomm_project.conf
+fi
 a2ensite ecomm_project.conf
 echo "Restart Apache"
 echo "######################################################"
