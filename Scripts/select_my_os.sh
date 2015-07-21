c4ff8f1b855fc2f8b7fb8398fd6d7030240c8f41
ashishkamat <ashishka@cybage.com>
2015-07-21 04:38:49 -0700

    Optimized Master Script and OS Detection Script

--- Scripts/select_my_os.sh
+++ Scripts/select_my_os.sh
@@ -1,19 +1,17 @@
 user="$(whoami)"
 echo $user
+uname -a;
 if (cat /etc/*-release|grep -iq CentOS)
 	then
 		echo "Centos is detected \n\n";
 		cd /home/$user/Scripts/centos
-		sh configuration.sh
-		sh ecommerce-deploy.sh
+		sh $1;		
 		#sh ecommerce-vhost.sh
 	elif (cat /etc/*-release|grep -iq Ubuntu)
 	then
 		echo "Ubuntu is detected \n\n";
-		ifconfig
 		cd /home/$user/Scripts/ubuntu
-		sh configuration.sh
-		sh ecommerce-deploy.sh
+		sh $1;		
 		#sh ecommerce-vhost.sh
 	else 
 		echo "Some other";
--- sshconnect.sh
+++ sshconnect.sh
@@ -1,16 +1,22 @@
 FILENAME=$1;
-global_user="";
+global_user=""
+message="Detecting OS"
+temp="sh /home/$username/Scripts/select_my_os.sh $3;"
 while read -r ip username password
 do
-	printf "*********************";
-	printf "Connecting to %s"$ip;
-	printf "********************\n\n";
-	export SSHPASS=$password
-	temp="sh /home/$username/Scripts/select_my_os.sh"
-	sshpass -e scp -r Scripts ecomm_project.zip $username@$ip:/home/$username
-	echo "Detecting Operating System\n\n"
-	echo $temp|sshpass -p $password ssh $username@$ip 
-	#echo $ip;
-	#break;
-	#echo "Hello World from Ashish Kamat">>/home/$username/Desktop/; exit
+	if [ "$2" = "copy" ]
+	then
+		echo "Copying to Remote $ip"   		
+		export SSHPASS=$password
+		sshpass -e scp -r Scripts ecomm_project.zip $username@$ip:/home/$username
+	elif [ "$2" = "env" ]
+	then
+		echo "RUNNING ENVIRONMENT SETUP SCRIPTS $ip";		
+		echo "sh /home/$username/Scripts/select_my_os.sh $3"|sshpass -p$password ssh $username@$ip 
+	elif [ "$2" = "deploy" ]
+	then
+		echo "RUNNING APPLICATION DEPLOYMNET SCRIPTS";		
+		echo "sh /home/$username/Scripts/select_my_os.sh $3"|sshpass -p$password ssh $username@$ip 
+	fi
+
 done  <$FILENAME
