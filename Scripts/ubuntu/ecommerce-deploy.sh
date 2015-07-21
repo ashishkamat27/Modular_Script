9dc371ee9852fb314f4fd8dacd67f5a7d7939b1f
ashishkamat <ashishka@cybage.com>
2015-07-21 07:00:02 -0700

    Modified Thinking

--- Scripts/ubuntu/ecommerce-deploy.sh
+++ Scripts/ubuntu/ecommerce-deploy.sh
@@ -36,28 +36,7 @@ echo  "127.0.0.1 ecomm_project">>/etc/hosts
 echo "##################################################"
 echo "Virtual Host Configuration"
 echo "###################################################"
-if [ -f /etc/apache2/sites-available/ecomm_project.conf ]; then
-echo  "File is already exist"
-else
-
-echo  '<VirtualHost *:80>
-    		<Directory "/var/www/html/ecomm_project">
-        	Options Indexes FollowSymLinks Includes ExecCGI
-        	Order allow,deny
-        	Allow from all
-    		</Directory>
-    		<IfModule mod_php5.c>
-        		php_admin_flag safe_mode Off
-        		php_admin_value register_globals 0
-        		php_value magic_quotes_gpc 0
-        		php_value magic_quotes_runtime 0
-        		php_value allow_call_time_pass_reference 0
-   		</IfModule>
-    		DirectoryIndex index.php
-    		DocumentRoot "/var/www/html/ecomm_project"
-    		ServerName ecomm_project
-   	</VirtualHost>'>>/etc/apache2/sites-available/ecomm_project.conf
-fi
+	sh ecommerce-vhost.sh
 echo "Restart Apache"
 echo "######################################################"
  /etc/init.d/apache2 reload
